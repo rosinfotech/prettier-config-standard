fa67bdb80422f6ec343184dd89becd309d778418
rosinfotech <rosinfo.tech@gmail.com>
2023-07-22 20:29:06 +0300

    0.0.6

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ module.exports = {
     "insertPragma": false,
     "jsxBracketSameLine": false,
     "jsxSingleQuote": false,
-    "printWidth": 80,
+    "printWidth": 100,
     "proseWrap": "preserve",
     "quoteProps": "as-needed",
     "requirePragma": false,
     "semi": true,
     "singleQuote": false,
     "tabWidth": 4,
-    "trailingComma": "es5"
+    "trailingComma": "es5",
+    "useTabs": false,
 }
